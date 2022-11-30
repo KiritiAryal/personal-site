7bec5659728c1673eeedd150b914c5c954dce430
KiritiAryal <karyal@ttu.edu>
2022-11-30 12:29:20 -0600

    Initial setup with required components, routes, and svg components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme, LightTheme } from "./components/Theme";
+import GlobalStyle from "./globalStyles";
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import WorkPage from "./components/WorkPage";
+import Skills from "./components/MySkillsPage";
+import Blog from "./components/BlogPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={LightTheme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/work" component={WorkPage} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+      </ThemeProvider>
+    </>
   );
 }
 
