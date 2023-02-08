599efc0bbc3d2cf4096d274cc3b250d93d1a2211
KiritiAryal <karyal@ttu.edu>
2023-02-07 20:11:27 -0600

    Trying for SEO

--- src/App.js
+++ src/App.js
@@ -38,44 +38,45 @@ function App() {
             content="Looking for a full-stack developer. Hire from a trusted University like Texas Tech and hire to diversify your workforce. We all like rainbows because they have more colors."
           />
         </Helmet>
-        <GlobalStyle />
-
-        <ThemeProvider theme={lightTheme}>
-          <SoundBar />
-
-          <LocomotiveScrollProvider
-            options={{
-              smooth: true,
-              // ... all available Locomotive Scroll instance options
-              smartphone: {
-                smooth: true,
-              },
-              tablet: {
-                smooth: true,
-              },
-            }}
-            watch={
-              [
-                //..all the dependencies you want to watch to update the scroll.
-                //  Basicaly, you would want to watch page/location changes
-                //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-              ]
-            }
-            containerRef={containerRef}
-          >
-            <AnimatePresence>{Loaded ? null : <Loader />}</AnimatePresence>
-            <main className="App" data-scroll-container ref={containerRef}>
-              <ScrollTriggerProxy />
-              <AnimatePresence>
-                {Loaded ? null : <Loader />}
-                <Main key="main" />
-                <About key="about" />
-                <WorkPage key="work" />
-              </AnimatePresence>
-            </main>
-          </LocomotiveScrollProvider>
-        </ThemeProvider>
       </HelmetProvider>
+
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <SoundBar />
+
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+            smartphone: {
+              smooth: true,
+            },
+            tablet: {
+              smooth: true,
+            },
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <AnimatePresence>{Loaded ? null : <Loader />}</AnimatePresence>
+          <main className="App" data-scroll-container ref={containerRef}>
+            <ScrollTriggerProxy />
+            <AnimatePresence>
+              {Loaded ? null : <Loader />}
+              <Main key="main" />
+              <About key="about" />
+              <WorkPage key="work" />
+            </AnimatePresence>
+          </main>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
     </>
   );
 }
