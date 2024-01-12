5a125650d947750e004d0d9975d988ae38986eba
KiritiAryal <karyal@ttu.edu>
2024-01-11 20:07:17 -0600

    Changed resume icon and corrected @media

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import ResumeIcon from "../assets/Images/cv.png";
 import "./About.css";
 const Card = styled.div`
   width: 600px;
-  height: 60px;
+  height: 65px;
   background-color: whitesmoke;
   border-radius: 4px;
   display: flex;
@@ -31,12 +31,6 @@ const BigText = styled.h1`
   }
 `;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-`;
 const Text = styled.h1`
   width: 100%;
   font-size: 22px;
@@ -49,54 +43,54 @@ const Text = styled.h1`
 function About1() {
   return (
     <>
-      <section className='about' id='about'>
-        <div className='about-text'>
-          <BigText>About</BigText>
-          <p>
-            I'm drawn to technology because it's this incredible canvas where I
-            can blend my technical prowess with a splash of creativity to tackle
-            intriguing challenges. Diversity is my jam – just like a rainbow
-            loses its magic with one color, I thrive in varied environments.
-            Learning is my perpetual quest, and every resource invested in me is
-            met with gratitude and a commitment to give back. I see the work
-            environment as a crucial element, and I'm fully devoted to doing my
-            part in nurturing it within any organization. I'm super excited
-            about the idea of bringing my skills and limitless potential for
-            growth to your awesome company.
-            <br />
-            <br />
-            I'm not just about the professional life, though. I bring a ton of
-            passion to everything I do, always pushing myself to be the best
-            version I can be.
-            <br />
-            <br />
-            I am grateful to myself for taking a fantastic two-month break for
-            some post-graduation travel adventures. In my downtime, you'll find
-            me out in nature, hiking and soaking up the great outdoors. I'm also
-            a bit of a culture enthusiast, always eager to dive into new
-            experiences and learn from different walks of life. Oh, and let's
-            not forget my love for the open road – nothing beats the thrill of a
-            good motorcycle ride!
-            <br />
-            <br />
-            One significant aspect of giving back has been my volunteer
-            experiences. I've dedicated my time to organizations like Feed My
-            Starving Children (FMSC), where I contributed to the noble cause of
-            providing meals to hungry kids, and Computers for the Blind (CFTB),
-            where I played a role in making technology accessible to visually
-            impaired individuals. These experiences have not only enriched my
-            understanding of community service but have also allowed me to apply
-            my technical skills in meaningful ways.
-            <br />
-            <br />
-            Dropping some fun facts about me! Started from humble beginnings,
-            funded my own adventures, ticking off two-thirds of US states and 4
-            countries. Next up? Solo backpacking through Europe before my knees
-            and spine decide to retire!
-            <br />
-            Oh, and I'm fluent in a few languages:
-            <br />
-            <br />
+      <div className='about'>
+        <BigText>About</BigText>
+        <p>
+          I'm drawn to technology because it's this incredible canvas where I
+          can blend my technical prowess with a splash of creativity to tackle
+          intriguing challenges. Diversity is my jam – just like a rainbow loses
+          its magic with one color, I thrive in varied environments. Learning is
+          my perpetual quest, and every resource invested in me is met with
+          gratitude and a commitment to give back. I see the work environment as
+          a crucial element, and I'm fully devoted to doing my part in nurturing
+          it within any organization. I'm super excited about the idea of
+          bringing my skills and limitless potential for growth to your awesome
+          company.
+          <br />
+          <br />
+          I'm not just about the professional life, though. I bring a ton of
+          passion to everything I do, always pushing myself to be the best
+          version I can be.
+          <br />
+          <br />
+          I am grateful to myself for taking a fantastic two-month break for
+          some post-graduation travel adventures. In my downtime, you'll find me
+          out in nature, hiking and soaking up the great outdoors. I'm also a
+          bit of a culture enthusiast, always eager to dive into new experiences
+          and learn from different walks of life. Oh, and let's not forget my
+          love for the open road – nothing beats the thrill of a good motorcycle
+          ride!
+          <br />
+          <br />
+          One significant aspect of giving back has been my volunteer
+          experiences. I've dedicated my time to organizations like Feed My
+          Starving Children (FMSC), where I contributed to the noble cause of
+          providing meals to hungry kids, and Computers for the Blind (CFTB),
+          where I played a role in making technology accessible to visually
+          impaired individuals. These experiences have not only enriched my
+          understanding of community service but have also allowed me to apply
+          my technical skills in meaningful ways.
+          <br />
+          <br />
+          Dropping some fun facts about me! Started from humble beginnings,
+          funded my own adventures, ticking off two-thirds of US states and 4
+          countries. Next up? Solo backpacking through Europe before my knees
+          and spine decide to retire!
+          <br />
+          Oh, and I'm fluent in a few languages:
+          <br />
+          <br />
+          <div className='container'>
             <div className='languages'>
               <Card>
                 <img
@@ -126,17 +120,14 @@ function About1() {
                 <Text>English</Text>
               </Card>
             </div>
-          </p>
-        </div>
-        <div className='about-img'>
-          <a href={Resume} target='_blank' className='nohover'>
-            <Container>
-              <img src={ResumeIcon} alt='ResumeIcon' height={10} width={20} />
-              <p>Resume</p>
-            </Container>
-          </a>
-        </div>
-      </section>
+            <div className='resume'>
+              <a href={Resume} target='_blank' className='nohover'>
+                <img src={ResumeIcon} alt='ResumeIcon' height={10} width={20} />
+              </a>
+            </div>
+          </div>
+        </p>
+      </div>
     </>
   );
 }
