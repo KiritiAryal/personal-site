93ac960e838fde2e930ee4a5ace03f05f5a9c793
KiritiAryal <karyal@ttu.edu>
2024-01-11 18:32:21 -0600

    Updated About Section

--- src/components/About.js
+++ src/components/About.js
@@ -53,21 +53,6 @@ function About1() {
         <div className='about-text'>
           <BigText>About</BigText>
           <p>
-            After taking a fantastic two-month break for some post-graduation
-            travel adventures, I'm now on the lookout for exciting full-time job
-            opportunities. In my downtime, you'll find me out in nature, hiking
-            and soaking up the great outdoors. I'm also a bit of a culture
-            enthusiast, always eager to dive into new experiences and learn from
-            different walks of life. Oh, and let's not forget my love for the
-            open road – nothing beats the thrill of a good motorcycle ride!
-            <br />
-            <br />
-            I'm not just about the fun stuff, though. I bring a ton of passion
-            to everything I do, always pushing myself to be the best version I
-            can be. Now, I'm super excited about the idea of bringing my skills
-            and unlimited potential for growth to your awesome company.
-            <br />
-            <br />
             I'm drawn to technology because it's this incredible canvas where I
             can blend my technical prowess with a splash of creativity to tackle
             intriguing challenges. Diversity is my jam – just like a rainbow
@@ -75,17 +60,33 @@ function About1() {
             Learning is my perpetual quest, and every resource invested in me is
             met with gratitude and a commitment to give back. I see the work
             environment as a crucial element, and I'm fully devoted to doing my
-            part in nurturing it within any organization.
+            part in nurturing it within any organization. I'm super excited
+            about the idea of bringing my skills and limitless potential for
+            growth to your awesome company.
+            <br />
+            <br />
+            I'm not just about the professional life, though. I bring a ton of
+            passion to everything I do, always pushing myself to be the best
+            version I can be.
+            <br />
+            <br />
+            I am grateful to myself for taking a fantastic two-month break for
+            some post-graduation travel adventures. In my downtime, you'll find
+            me out in nature, hiking and soaking up the great outdoors. I'm also
+            a bit of a culture enthusiast, always eager to dive into new
+            experiences and learn from different walks of life. Oh, and let's
+            not forget my love for the open road – nothing beats the thrill of a
+            good motorcycle ride!
             <br />
             <br />
             One significant aspect of giving back has been my volunteer
             experiences. I've dedicated my time to organizations like Feed My
             Starving Children (FMSC), where I contributed to the noble cause of
-            providing meals to those in need, and Computers for the Blind
-            (CFTB), where I played a role in making technology accessible to
-            visually impaired individuals. These experiences have not only
-            enriched my understanding of community service but have also allowed
-            me to apply my technical skills in meaningful ways.
+            providing meals to hungry kids, and Computers for the Blind (CFTB),
+            where I played a role in making technology accessible to visually
+            impaired individuals. These experiences have not only enriched my
+            understanding of community service but have also allowed me to apply
+            my technical skills in meaningful ways.
             <br />
             <br />
             Dropping some fun facts about me! Started from humble beginnings,
